f4862ce91d6246764007c216bfbadab82c91fd9d
Wale <onafesowale@gmail.com>
2024-07-15 06:21:08 +0100

    avif img test

--- src/components/AboutUs/Solution.js
+++ src/components/AboutUs/Solution.js
@@ -1,73 +1,95 @@
 function Solution () {
     return (
-        <section className="section-lgx">
-            <div className="container">
-                <div className="row g-0 align-items-center">
-                    <div className="col-md-12 col-xl-6">
-                        <div className="row">
-                            <div className="col-md-6">
-                                <div className="about-three_img1">
-                                    {/* <div className="pbmit-animation-style4"> */}
-                                    <div className="">
-                                        <img src="./assets/img/solution.avif" className="img-fluid" alt="ty"></img>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="about-three_img2">
-                                    {/* <div className="pbmit-animation-style3"> */}
-                                    <div className="">
-                                        <img src="./assets/img/happyman.avif" className="img-fluid" alt="tu"></img>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+      <section className="section-lgx">
+        <div className="container">
+          <div className="row g-0 align-items-center">
+            <div className="col-md-12 col-xl-6">
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="about-three_img1">
+                    {/* <div className="pbmit-animation-style4"> */}
+                    <div className="">
+                      <img
+                        src="./assets/img/solution.avif"
+                        className="img-fluid"
+                        alt="ty"
+                      ></img>
                     </div>
-                    <div className="col-md-12 col-xl-6">
-                        <div className="about-us-three-rightbox">
-                            <div className="pbmit-heading-subheading animation-style2">
-                                <h4 className="pbmit-subtitle">igoglobal helps</h4>
-                                <h2 className="pbmit-title">provide the best health and wellness products</h2>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="about-three_img2">
+                    {/* <div className="pbmit-animation-style3"> */}
+                    <div className="">
+                      <img
+                        src="https://igogolobal-assets.s3.eu-west-2.amazonaws.com/images/happyman.avif"
+                        className="img-fluid"
+                        alt="tu"
+                      ></img>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-12 col-xl-6">
+              <div className="about-us-three-rightbox">
+                <div className="pbmit-heading-subheading animation-style2">
+                  <h4 className="pbmit-subtitle">igoglobal helps</h4>
+                  <h2 className="pbmit-title">
+                    provide the best health and wellness products
+                  </h2>
+                </div>
+                <div className="about-us-three-content">
+                  <div className="pbmit-desc">
+                    Our approach at Igoglobal breaks the mold of traditional
+                    industry standards by recognizing that everyone is unique,
+                    with diverse backgrounds, tastes, and aspirations. Igoglobal
+                    is dedicated to embracing your individuality and unlocking
+                    your exceptional potential.{" "}
+                  </div>
+                  <div className="row mt-5 mb-5">
+                    <div className="col-md-6">
+                      <div className="pbmit-ihbox-style-2">
+                        <div className="pbmit-ihbox-icon">
+                          <div className="pbmit-ihbox-icon-wrapper">
+                            <div className="pbmit-icon-wrapper pbmit-icon-type-icon">
+                              <i className="pbmit-capigo-icon pbmit-capigo-icon-call-center"></i>
                             </div>
-                            <div className="about-us-three-content">
-                                <div className="pbmit-desc">Our approach at Igoglobal breaks the mold of traditional industry standards by recognizing that everyone is unique, with diverse backgrounds, tastes, and aspirations. Igoglobal is dedicated to embracing your individuality and unlocking your exceptional potential. </div>
-                                <div className="row mt-5 mb-5">
-                                    <div className="col-md-6">
-                                        <div className="pbmit-ihbox-style-2">
-                                            <div className="pbmit-ihbox-icon">
-                                                <div className="pbmit-ihbox-icon-wrapper">
-                                                    <div className="pbmit-icon-wrapper pbmit-icon-type-icon">
-                                                        <i className="pbmit-capigo-icon pbmit-capigo-icon-call-center"></i>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="pbmit-ihbox-contents">
-                                                <h2 className="pbmit-element-title">
-                                                    Customer Assistance
-                                                </h2>
-                                                <div className="pbmit-heading-desc">We have the best hands to help attend to all your various needs, don't hesitate to reach out to us</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-md-6">
-                                        <div className="pbmit-ihbox-style-2">
-                                            <div className="pbmit-ihbox-icon">
-                                                <div className="pbmit-ihbox-icon-wrapper">
-                                                    <div className="pbmit-icon-wrapper pbmit-icon-type-icon">
-                                                        <i className="pbmit-capigo-icon pbmit-capigo-icon-investment"></i>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="pbmit-ihbox-contents">
-                                                <h2 className="pbmit-element-title">
-                                                    Improved health
-                                                </h2>
-                                                <div className="pbmit-heading-desc">Our various natural extracted products are guaranteed to provide improved health.</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                {/* <a className="pbmit-btn" href="about-us.html">
+                          </div>
+                        </div>
+                        <div className="pbmit-ihbox-contents">
+                          <h2 className="pbmit-element-title">
+                            Customer Assistance
+                          </h2>
+                          <div className="pbmit-heading-desc">
+                            We have the best hands to help attend to all your
+                            various needs, don't hesitate to reach out to us
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="pbmit-ihbox-style-2">
+                        <div className="pbmit-ihbox-icon">
+                          <div className="pbmit-ihbox-icon-wrapper">
+                            <div className="pbmit-icon-wrapper pbmit-icon-type-icon">
+                              <i className="pbmit-capigo-icon pbmit-capigo-icon-investment"></i>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="pbmit-ihbox-contents">
+                          <h2 className="pbmit-element-title">
+                            Improved health
+                          </h2>
+                          <div className="pbmit-heading-desc">
+                            Our various natural extracted products are
+                            guaranteed to provide improved health.
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  {/* <a className="pbmit-btn" href="about-us.html">
                                     <span className="pbmit-button-content-wrapper">
                                         <span className="pbmit-button-icon pbmit-align-icon-right">
                                             <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" className="pbmit-svg-arrow" x="0px" y="0px" width="10" height="19" viewBox="0 0 19 19" xmlSpace="preserve">
@@ -79,13 +101,13 @@ function Solution () {
                                         <span className="pbmit-button-text">Learn More</span>
                                     </span>
                                 </a>  */}
-                            </div>
-                        </div>
-                    </div>
                 </div>
+              </div>
             </div>
-        </section>
-    )
+          </div>
+        </div>
+      </section>
+    );
 }
 
 export default Solution;
